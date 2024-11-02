c936846d9263ffd96dcf800f631eec778137bf3d
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2024-11-01 20:47:18 -0500

    retry

--- util/ray.py
+++ util/ray.py
@@ -3,7 +3,7 @@ from ray.util.state import summarize_tasks
 
 def success(counter):
     finished = 0
-    summary = summarize_tasks()['cluster']['summary']
+    summary = summarize_tasks(timeout=60, _explain=True)['cluster']['summary']
     print(summary)
     for value in summary.values():
         if 'FINISHED' in value['state_counts']:
